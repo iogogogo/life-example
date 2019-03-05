ebbe391403b015baf096a0a9183ebebba8741619
tao.zeng <tao.zeng9@gmail.com>
2019-03-05 19:33:24 +0800

    m

---
+++ example-jackson/src/main/java/com/example/model/Model.java
@@ -0,0 +1,20 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Model implements Serializable {
+
+    private String name;
+
+    private double weight;
+
+    private LocalDateTime localDateTime;
+}
